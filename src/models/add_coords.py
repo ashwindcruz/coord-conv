7239ac63f1f7e0c719bf7e691697f21baf679136
CodeOwl94 <ashwin@bloom.org.au>
2019-03-19 22:57:24 +0000

    Use the method provided in the paper to add coordinate layers

--- src/models/add_coords.py
+++ src/models/add_coords.py
@@ -10,17 +10,37 @@ def add_coords_layers(input_tensor):
 		output_tensor: Similar to input tensor but with two additional layers
 			specifying xy coordinates. 
 	"""
-	row_dim = input_tensor.get_shape().as_list()[1]
-	col_dim = input_tensor.get_shape().as_list()[2]
+	batch_size_tensor = tf.shape(input_tensor)[0]
+	x_dim = tf.shape(input_tensor)[1]
+	y_dim = tf.shape(input_tensor)[2]
+	xx_ones = tf.ones([batch_size_tensor, x_dim], dtype=tf.int32)
+	xx_ones = tf.expand_dims(xx_ones, -1)
+	xx_range = tf.tile( 
+		tf.expand_dims(tf.range(y_dim), 0), [batch_size_tensor, 1])
+	xx_range = tf.expand_dims(xx_range, 1)
+	xx_channel = tf.matmul(xx_ones, xx_range)
+	xx_channel = tf.expand_dims(xx_channel, -1)
+	yy_ones = tf.ones([batch_size_tensor, y_dim], dtype=tf.int32)
+	yy_ones = tf.expand_dims(yy_ones, 1)
+	yy_range = tf.tile(
+		tf.expand_dims(tf.range(x_dim), 0), [batch_size_tensor, 1])
+	yy_range = tf.expand_dims(yy_range, -1)
+	yy_channel = tf.matmul(yy_range, yy_ones)
+	yy_channel = tf.expand_dims(yy_channel, -1)
 
-	row_inds = tf.linspace(0.0, row_dim-1, row_dim)
-	col_inds = tf.linspace(0.0, col_dim-1, col_dim)
+	x_dim = tf.cast(x_dim, tf.float32)
+	y_dim = tf.cast(y_dim, tf.float32)
 
-	row_matrix, col_matrix = tf.meshgrid(row_inds, col_inds)
+	xx_channel = tf.cast(xx_channel, tf.float32) / (x_dim - 1)
+	yy_channel = tf.cast(yy_channel, tf.float32) / (y_dim - 1)
+	xx_channel = xx_channel*2 - 1
+	yy_channel = yy_channel*2 - 1
 
-	row_matrix = row_matrix[None, :, :, None]
-	col_matrix = col_matrix[None, :, :, None]
 
-	output_tensor = tf.concat([input_tensor, row_matrix, col_matrix], axis=-1)
+
+	ret = tf.concat([input_tensor, xx_channel, yy_channel], axis=-1)
+
+	output_tensor = ret
+	
 
 	return output_tensor
